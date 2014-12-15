256d7b2527cdd04449e9c9d434f479d41bbbf91a
Michael Cerezo <cerezo.michael@gmail.com>
2014-12-14 18:35:49 -0800

    my blog work in progress

--- view/header.php
+++ view/header.php
@@ -6,7 +6,56 @@
  -->
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<title></title>
+	<link rel="stylesheet" type="text/css" media="screen and (min-width: 0px)" href="css/small.css">
+	<link rel="stylesheet" type="text/css" media="screen and (min-width: 100px)" href="css/medium.css">
+	<link rel="stylesheet" type="text/css" media="screen and (min-width: 1400px)" href="css/large.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<title>My Blog</title>
 </head>
 <body>
+	<section id="home" data-type="background" data-speed="10" class="pages"> 
+		<p><br><center><h1><img id="img" src="images/images.jpeg"></h1></center></p>   
+        <article><p><br><br><br><br><br><br><h1><center>MICHAEL</center></h1></p>
+		<p><h1><center>cerezo</center></h1></p></article>
+    </section>   
+ 
+	<section id="about" data-type="background" data-speed="10" class="pages">
+         <article>
+         	<p><h1><center>About Me!</center></h1></p>
+
+         	<!-- Button trigger modal -->
+			<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  				<img id="img1" src="images/me1.png" width="200px" height="200px">
+			</button>
+
+<!-- Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  					<div class="modal-dialog">
+    					<div class="modal-content">
+            				<h4 class="modal-title" id="myModalLabel">About Me!!!</h4>
+      							<div class="modal-body">
+      								Crucifix roof party Austin, slow-carb sriracha Portland selvage XOXO salvia. Farm-to-table kogi umami, +1 four dollar toast Vice PBR pickled distillery street art 90's Portland letterpress. Schlitz High Life Williamsburg sartorial distillery ennui. 8-bit wolf pour-over, normcore post-ironic jean shorts mixtape semiotics Tumblr quinoa Truffaut raw denim retro skateboard cold-pressed. Four loko single-origin coffee meggings Wes Anderson flannel, asymmetrical blog Tumblr tofu fixie lomo cray cred listicle ennui. Small batch meh gentrify Williamsburg Banksy umami. Vegan viral Bushwick, cray keffiyeh cliche hashtag small batch.
+
+Blue Bottle flannel kogi, leggings deep v meggings Brooklyn put a bird on it ugh taxidermy viral VHS. Bushwick mumblecore crucifix, American Apparel cliche tattooed direct trade roof party. IPhone normcore dreamcatcher, four loko Kickstarter hashtag mumblecore selvage tofu photo booth jean shorts art party lumbersexual. Four loko banjo Godard pour-over disrupt pop-up, brunch actually photo booth. Bushwick locavore Pitchfork Pinterest small batch, pickled messenger bag American Apparel keffiyeh chambray slow-carb next level. Fashion axe XOXO viral brunch put a bird on it slow-carb. Chillwave actually four dollar toast hashtag letterpress, gastropub semiotics 90's Shoreditch Echo Park cray PBR&B.
+
+Shabby chic American Apparel biodiesel flexitarian mumblecore locavore. Artisan cliche brunch, drinking vinegar Shoreditch mumblecore kitsch freegan jean shorts put a bird on it. Typewriter McSweeney's cliche Pinterest before they sold out vegan. Cred 8-bit letterpress next level. Brunch art party Pitchfork, scenester raw denim pickled lumbersexual fanny pack photo booth Tumblr messenger bag master cleanse. Sustainable Odd Future aesthetic pug pop-up. Wayfarers Godard Brooklyn, blog Schlitz selfies bitters Marfa hoodie polaroid.
+
+Pickled next level YOLO hella. Shoreditch Tumblr small batch, American Apparel hashtag 90's PBR&B banjo scenester meh swag fap. DIY dreamcatcher Echo Park, distillery slow-carb fixie farm-to-table four loko gluten-free. Next level selfies freegan, +1 readymade trust fund pickled keffiyeh skateboard. Master cleanse normcore Williamsburg freegan Etsy. Thundercats freegan Schlitz, wayfarers selfies Wes Anderson semiotics gluten-free artisan squid fanny pack. Mixtape deep v distillery blog Wes Anderson PBR literally, next level polaroid chia flannel.
+
+Typewriter gluten-free four dollar toast hella YOLO butcher. Actually blog single-origin coffee High Life church-key Banksy. Pug Odd Future +1 gastropub distillery direct trade. Fap Marfa gastropub salvia, Shoreditch fashion axe Odd Future migas yr. Letterpress mumblecore Schlitz tilde hoodie. Lo-fi wolf DIY leggings, paleo cornhole swag pour-over sriracha. Cornhole wolf mlkshk before they sold out, polaroid organic High Life VHS church-key Etsy chillwave tofu.
+
+Austin master cleanse hoodie High Life paleo lumbersexual. Tilde Wes Anderson beard XOXO literally. Next level try-hard keffiyeh gentrify stumptown lo-fi disrupt. Small batch Thundercats bitters, pug tote bag craft beer retro fingerstache butcher authentic High Life. Next level McSweeney's Intelligentsia, letterpress squid listicle organic Carles. Single-origin coffee flexitarian Bushwick, Neutra flannel street art messenger bag lomo umami put a bird on it food truck wolf butcher cornhole actually. Lomo mixtape roof party raw denim leggings hoodie.
+
+Actually post-ironic art party, cold-pressed street art YOLO cliche shabby chic cronut paleo freegan occupy XOXO. Cardigan readymade distillery asymmetrical pork belly, farm-to-table church-key plaid photo booth. Kale chips viral biodiesel cold-pressed Neutra artisan. Bushwick food truck beard biodiesel. Truffaut keffiyeh fashion axe, migas quinoa irony lomo mlkshk. Lumbersexual crucifix retro, Echo Park whatever McSweeney's authentic letterpress. Etsy McSweeney's flannel seitan.
+Oh. You need a little dummy text for your mockup? How quaint.
+
+I bet you’re still using Bootstrap too…
+      						</div>
+    				</div>
+  				</div>
+			</div>
+		</article>
+	</section>
